5a6b4016646f45263cb40b29e6616b3fb6af4a1f
Ian <Iokeef@gmail.com>
2017-06-07 12:26:44 -0400

    Add more to addwidget

--- app/components/add_widget/add_widget.jsx
+++ app/components/add_widget/add_widget.jsx
@@ -6,13 +6,34 @@ export default class AddWidget extends React.Component {
   constructor(props) {
     super(props);
     this.update = this.update.bind(this);
-    this.state = {title: "", description: ""};
+    this.renderErrors = this.renderErrors.bind(this);
+    this.state = {title: "", description: "", errors: null};
   }
   
   update(updatedArea) {
     return (e) => {
-      this.setState({[updatedArea]: e.target.value});
+      let charLimit = updatedArea === "title" ? 25 : 50;
+      if(this.verifyLength(e.target.value, charLimit)) {
+        this.setState({[updatedArea]: e.target.value, errors: null});
+      }
+      else {        
+        this.setState({errors: `Please use less than ${charLimit} character for your ${updatedArea}.`});
+      }
     }
+  }
+  
+  renderErrors() {
+    if(this.state.errors) {
+      return( 
+        <div className="error-message">
+          {this.state.errors}
+        </div>
+      )
+    }
+  }
+  
+  verifyLength(input, charLimit) {
+    return input.length < charLimit;
   }
 
   render() {
@@ -41,12 +62,14 @@ export default class AddWidget extends React.Component {
         </div>
         <div className="widget-options">
           <h3>Widget Header (Optional)</h3>
+          {this.renderErrors()}
           <h2>Widget Title (25 characters max)</h2>
           <input type="text" value={this.state.title}  onChange={this.update('title')} ></input>
           <h2>Widget Description (50 characters max)</h2>
           <textarea value={this.state.description} onChange={this.update('description')} />
         </div>
         <div className="widget-confirmation">
+          
         </div>
       </div>
     );
